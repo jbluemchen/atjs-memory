ec63caf3c33838ff14d417b3562b546ab0be25e5
jbluemchen <julia.blum0@gmail.com>
2023-02-15 16:04:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+export default {
+  name: 'App'
+}
+
+</script>
+<template>
+  <h1>Sound memory</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
+</template>
+
+
+<style>
+#app {
+  font-family: 'Courier New', Courier, monospace;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
