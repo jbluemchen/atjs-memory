e92895bef6018835d1d0eef6799ac3171a112b57
jbluemchen <julia.blum0@gmail.com>
2023-02-15 16:23:30 +0100

    initializing cards and game board

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup>
-
+<script>
+export default{
+  name: 'App'
+}
 </script>
+
 <template>
   <h1>Sound memory</h1>
   <section class="game-board">
@@ -24,5 +27,16 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.card {
+  border: 5px solid #ccc
+}
+.game-board{
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
 }
 </style>
